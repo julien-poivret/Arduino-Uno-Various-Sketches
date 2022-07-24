b4cdb100bd76f528dfb1b6bc11e5241d95024217
Poivret Julien <80133330+julien-poivret@users.noreply.github.com>
2022-07-24 22:31:42 +0200

    Add files via upload

---
+++ General_purpose_timing/General_purpose_timing.ino
@@ -0,0 +1,23 @@
+/*
+
+       General purpose Timing Method
+
+*/
+
+uint32_t prev_milliseconds = 0;
+uint32_t current_milliseconds = 0;
+uint32_t _delay = 500; // In ms
+
+void setup(){
+	DDRB |= 0x20;  // PB5 OUTPUT.
+}
+
+void loop(){
+	current_milliseconds = millis(); //Update Cursor
+	if((current_milliseconds - prev_milliseconds) >= _delay){ // check cursor from target delay
+		PORTB ^= 0x20; // toggle PB5.
+		prev_milliseconds = current_milliseconds; // reset the cursor to start referance.
+	}
+
+	// Serial.println(prev_milliseconds);
+}
