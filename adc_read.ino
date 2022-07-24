2f8a4968b74752c86d8e13bbf0df1296e0f31530
Poivret Julien <80133330+julien-poivret@users.noreply.github.com>
2022-07-24 02:13:41 +0200

    Update adc_read.ino

--- adc_read.ino
+++ adc_read.ino
@@ -2,12 +2,29 @@
        Config the 10 bits ADC (raw study.)
        -> the purpose is to understand the peripheral of the chip 
        in order to build a "driver" for the peripheral. 
-       ( in a practical a use, an AnalogRead may do the trick most of the time )
+       ( in a practical use, an AnalogRead may do the trick, most of the time...)
        -> but better to understand hardware in order to track it's technological 
-       evolution and features advantage,
-       when it will be time to work one a more advanced chip. 
+       evolution and features advantage, specialy when it will be time to work one a more advanced chip technologie. 
+
+    
+       Convert an analog signal in raw and succesives digital values.
+    
+^   sin x
+     |
+    ,|, 1    _....._
+     |    ,="       "=.
+     |  ,"             ".                           ,"
+     |,"        ,        ".,          ,          ,,"
+"""".*""""""""""|""""""""""|."""""""""|""""""""".|"""">
+  ." |                       ".               ." __    x
+,"   |                         "._         _,"   ||
+    ,|,                  sm       "-.....-"
+     |  -1
+     |
+
 */
 
+// Arduino Uno Analog Digital Converter registers deffinition.
 
 volatile uint8_t* _ADMUX = (volatile uint8_t*) 0x7C;   // Multiplexer
 volatile uint8_t* _ADCSRA = (volatile uint8_t*) 0x7A;  // Status register A
@@ -37,14 +54,14 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
+  // put your main code here, to run repeatedly :
   PORTB ^= 0x1;
   delay(100);
   Serial.println(_ADC_read(),Dec);
 }
 
 uint16_t _ADC_read(void){
-  *_PRR &= ~0x1; // disable power reduction to perform a read.
+  *_PRR &= ~0x1; // Disable power reduction to perform a read.
   *_ADCSRA |= 0x7; // Set the sampling rate at 16MHZ/128 sample /sec (125khz)
   *_ADCSRA |= 0xC0; // start 25 ADC conversion cycles. 
   PORTB |= 0x20; // led 13 On reading pending.
