e7a11b35e80ac3356409ef64bd3b86d0e7e96d6f
Poivret Julien <80133330+julien-poivret@users.noreply.github.com>
2022-08-01 20:16:33 +0200

    Working and Tested tx  rx routine

---
+++ USART_tx_rx_Arduino/Atmega328p_UART_Transmitter.ino
@@ -0,0 +1,112 @@
+
+/*
+
+      This is the sender. 
+      ( it's send a series of bytes with a  predifined time intervals ).
+
+*/
+
+
+/////////////////////////             Define peripherals registers.
+
+volatile uint8_t* _SER = (volatile uint8_t *) 0x5F;     //      Global Interrupt
+
+volatile uint8_t* _UDR0 = (volatile uint8_t *) 0xC6;    //      Data Register
+
+volatile uint8_t* _UCSR0A = (volatile uint8_t *) 0xC0;  //      Control and status register A
+volatile uint8_t* _UCSR0B = (volatile uint8_t *) 0xC1;  //      Control and status register B
+volatile uint8_t* _UCSR0C = (volatile uint8_t *) 0xC2;  //      Control and status register C
+
+volatile uint8_t* _UBRR0H = (volatile uint8_t *) 0xC5;  //     Baud Rate Hight register
+volatile uint8_t* _UBRR0L = (volatile uint8_t *) 0xC4;  //     Baud Rate low register
+
+
+
+//////////////////////////////////////////////////////////// UART0
+/*
+   init UART0 with two possibles settings: 9600 or 115200.
+*/
+void UART0_init(uint16_t BaudRate) {
+  *_SER &= ~0x80;                                      // Disable global interrupt.
+
+  *_UCSR0C &= ~0xC;                                    // Set asynchronous mode.
+  *_UCSR0A |= 0x2;                                     // Set the (FULL speed) for asynchronous mode.
+
+  // Set the Baudrate. ----->  baud_setting = (F_CPU / 4 / baud - 1) / 2
+  if (BaudRate == 9600) {
+    *_UBRR0H &= ~ 0xF;
+    *_UBRR0L = 0xCF;
+  } else if (BaudRate == 115200) {
+    *_UBRR0H &= ~ 0xF;
+    *_UBRR0L = 0x10;
+  }
+
+  // Set the frame in 8 bits mode.
+  *_UCSR0C |= 0x6;
+  *_UCSR0C &= ~0x8; // (1 stop bit).
+
+  // Enable Rx Tx mode + 8 bits mode part2.
+  *_UCSR0B = 0x18;
+
+  *_SER |= 0x80; // Enable global interrupt.
+}
+
+/*
+   Send data with exact predefined length (for speed).
+*/
+void UART_send_fast(char* asci_data, uint8_t length) {
+  *_UCSR0B |= 0x8; //Tx Enabled.
+  for (uint8_t i = 0; i < length; i++) {
+    while (!(*_UCSR0A & 0x20)) { // wait for prev tx to complete
+      asm("");
+    }
+    *_UDR0 = *(asci_data + i); // ready!, so write the next stacked bytes on buffer.
+  }
+}
+/*
+   Send data with automatic stack length (slower).
+*/
+void UART_send(char* asci_data) {
+  *_UCSR0B |= 0x8; //Tx Enabled.
+  uint8_t ct = 0;
+  while (1) {
+    while (!(*_UCSR0A & 0x20)) { // wait for prev tx to complete
+      asm("");
+    }
+    if (*(asci_data + ct)) {    // if not a terminating bytes (0).
+      *_UDR0 = *(asci_data + ct); //  write the next stacked bytes on buffer.
+      ct++;
+    } else {
+      break;
+    }
+  }
+}
+void UART_receive(char* asci_data, uint8_t length, uint8_t* Error_flag) {
+  uint16_t ct = 0;
+  for (uint8_t i = 0; i < length; i++) {
+    ct=0;
+    while( (!(*_UCSR0A & (1 << 7)) && (*_UCSR0A&(1<<5) ))) {
+      if (ct >= 0xF0) { // if time out is reach => Error !
+        *Error_flag = 1;// Timout must be adjusted...
+        break;
+      }
+      ct++;
+    }
+    *(asci_data + i) = *_UDR0;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
+char data_rx[24] = {0};
+
+
+void setup() {
+  // put your setup code here, to run once:
+  UART0_init(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  UART_send_fast("Non private message !!!\n", 24);
+  delay(500);
+
+}
